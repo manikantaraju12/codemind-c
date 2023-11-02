ae962d0cde3a86d592b1615e0cd52a35e3f46f0f
manikantaraju12 <147791988+manikantaraju12@users.noreply.github.com>
2023-11-02 20:42:29 +0530

    #include<stdio.h> int main() {     char x;     scanf("%c",&x);     switch(x)     {         case'V':printf("Violet");         break;         case'I':printf("Indigo");         break;         case'B':printf("Blue");         break;         case'G':printf("Green");         break;         case'Y':printf("Yellow");         break;         case'O':printf("Orange");         break;         case'R':printf("Red");         break;         default:printf("-1");
        }
    }

---
+++ Print_the_color_name.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    char x;
+    scanf("%c",&x);
+    switch(x)
+    {
+        case'V':printf("Violet");
+        break;
+        case'I':printf("Indigo");
+        break;
+        case'B':printf("Blue");
+        break;
+        case'G':printf("Green");
+        break;
+        case'Y':printf("Yellow");
+        break;
+        case'O':printf("Orange");
+        break;
+        case'R':printf("Red");
+        break;
+        default:printf("-1");
+        
+        
+    }
+}
