a370b5c9f325b2c038a53bb146b730c2cc2eb50c
manikantaraju12 <147791988+manikantaraju12@users.noreply.github.com>
2023-11-23 20:40:51 +0530

    #include<stdio.h> int main() {     int m,n,s;     scanf("%d%d",&m,&n);     s=m*n;     printf("%d",s); }

--- Instant_Noodles.c
+++ Instant_Noodles.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int x,y,z;
-    scanf("%d%d",&x,&y);
-    z=x*y;
-    printf("%d",z);
+    int m,n,s;
+    scanf("%d%d",&m,&n);
+    s=m*n;
+    printf("%d",s);
 }
