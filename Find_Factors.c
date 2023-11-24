f28944b799adf651218e37d5e244852578686e3d
manikantaraju12 <147791988+manikantaraju12@users.noreply.github.com>
2023-11-24 09:51:10 +0530

    #include<stdio.h> int main() {     int x,i;     scanf("%d",&x);     for(i=1;i<=x;i++){         if(x%i==0)     {     printf("%d ",i); } } }

--- Find_Factors.c
+++ Find_Factors.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-int main() 
+#include<stdio.h>
+int main()
 {
     int x,i;
     scanf("%d",&x);
-    for (i = 1; i <=x; ++i)
+    for(i=1;i<=x;i++){
+        if(x%i==0)
     {
-        if (x % i == 0)
-        {
-            printf("%d ", i);
-        }
-    }
+    printf("%d ",i);
+}
+}
 }
