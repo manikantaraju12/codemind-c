5e1e5c6b28c514f97d0169cf5c07c4ac4f074f2d
manikantaraju12 <147791988+manikantaraju12@users.noreply.github.com>
2023-12-22 21:45:35 +0530

    #include<stdio.h> int main() {     int n,i,d;     static int a[10];     scanf("%d",&n);     while(n>0){         d=n%10;         a[d]++;         n=n/10;     }     for(i=0;i<10;i++){         if(a[i]>1){         printf("Not Unique Number");         break;     } }     if(i==10){         printf("Unique Number");     }
    }

---
+++ Unique_number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,d;
+    static int a[10];
+    scanf("%d",&n);
+    while(n>0){
+        d=n%10;
+        a[d]++;
+        n=n/10;
+    }
+    for(i=0;i<10;i++){
+        if(a[i]>1){
+        printf("Not Unique Number");
+        break;
+    }
+}
+    if(i==10){
+        printf("Unique Number");
+    }
+    
+}
